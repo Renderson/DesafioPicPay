6813bd8ec3b9e369b23d487d4e82d30fe9150552
Renderson <renderson.silva@sodresantoro.com.br>
2020-03-03 16:08:02 -0300

    criado telas de navegação

---
+++ app/src/main/java/com/renderson/desafiopicpay/presentation/contacts/ContactActivity.kt
@@ -0,0 +1,13 @@
+package com.renderson.desafiopicpay.presentation.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.renderson.desafiopicpay.R
+
+class ContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_main)
+    }
+}
